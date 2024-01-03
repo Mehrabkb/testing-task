1a374c3d137be7dc17397bdc045aaf119eb70f54
mehrabkb <mkordbacheh@gmail.com>
2024-01-03 14:04:12 +0330

    header works

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-  for(let i = 1 ; i <= 295 ; i++){
+  for(let i = 1 ; i <= 320 ; i++){
     $('.circles').append(`<div class="item"></div>`);
   }
+
+  $('.top .main .header .items .item').click(function(){
+    let that = $(this);
+    let items = document.querySelectorAll('.top .main .header .items .item');
+    items.forEach((result)=>{
+      result.classList.remove('active');
+    });
+    that.addClass('active');
+  })
